bc3f1e3e258e341974d1207bca38b38e92f214a6
fergaldoyle <fergal_doyle@yahoo.com>
2023-04-20 13:46:15 +0100

    Fix issue with envsubst and tee creating a blank file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,8 @@
 # Used in Docker file for ENTRYPOINT
 
 echo "$@"
-envsubst < /root/config_eo_service.yml | tee /root/config_eo_service.yml
+#envsubst < /root/config_eo_service.yml | tee /root/config_eo_service.yml # this causes config_eo_service.yaml to be blank sometimes, see https://stackoverflow.com/q/60546292
+FILE=/root/config_eo_service.yml
+tmp=$(mktemp)
+envsubst < "$FILE" > "$tmp" &&  mv "$tmp" "$FILE"
 python3 -W ignore -m eo_custom_scripts "$@"
